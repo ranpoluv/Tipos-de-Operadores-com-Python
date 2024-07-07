c83716f71cec8fc87001e29edb98dd67045e277e
Thamires <121324998+ranpoluv@users.noreply.github.com>
2024-07-07 20:52:08 -0300

    operadores_de_comparacao.py

---
+++ operadores-de-comparacao/operadores_de_comparacao.py
@@ -0,0 +1,20 @@
+saldo = 200
+saldo = 200
+
+# Igualdade, Resultado = True
+print(saldo == saque)
+
+# Diferença, Resultado = False
+print(saldo != saque)
+
+# Maior que, Resultado = False
+print(saldo > saque)
+
+# Maior ou Igual, Resultado = True
+print(saldo >= saque)
+
+# Menor Que, Resultado = False
+print(saldo < saque)
+
+# Menor ou Igual, Resultado = True
+print(saldo <= saque)
