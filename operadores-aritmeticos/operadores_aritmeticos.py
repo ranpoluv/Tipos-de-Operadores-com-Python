4f76040acda275c234e3bbeef98019845c1a8d87
Thamires <121324998+ranpoluv@users.noreply.github.com>
2024-07-07 20:23:29 -0300

    operadores_aritmeticos.py

---
+++ operadores-aritmeticos/operadores_aritmeticos.py
@@ -0,0 +1,28 @@
+produtos_1 =20
+produtos_2 = 10
+
+# Adição, resultado = 30
+print(produtos_1 + produtos_2)
+
+# Subtração, resultado = 10
+print(produtos_1 - produtos_2)
+
+# Divisão, resultado = 2.0
+print(produtos_1 / produtos_2)
+
+# Divisão inteira, resultado = 2
+print(produtos_1 // produtos_2)
+
+# Multiplicação, resultado = 200
+print(produtos_1 * produtos_2)
+
+# Módulo, resultado = 0
+print(produtos_1 % produtos_2)
+
+# Exponenciação, resultado = 10240000000000
+print(produtos_1 ** produtos_2)
+
+# Resultado
+x = (10 + (5 * 4))
+y = (10 / 2) + (25 * 2) - (2 ** 2)
+print(x, y)
