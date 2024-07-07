67c59d1a0dcf426cd1cdb10acb0e72efe25dd324
Thamires <121324998+ranpoluv@users.noreply.github.com>
2024-07-07 20:28:41 -0300

    introducao.py

---
+++ operadores-de-comparacao/introducao.py
@@ -0,0 +1,24 @@
+# Igualdade
+saldo = 450
+saque = 200
+
+print(saldo == saque) # Resultado: False
+
+# Diferença
+saldo = 450
+saque = 200
+print(saldo != saque) # Resultado = True
+
+# Maior que / Maior ou igual
+saldo = 450
+saque = 200
+print(saldo > saque) # Resultado = True
+
+print(saldo >= saque) # Resultado = True
+
+# Menor que / Menor ou igual
+saldo = 450
+saque = 200
+print(saldo < saque) # Resultado = False
+
+print(saldo <= saque) # Resultado = False
