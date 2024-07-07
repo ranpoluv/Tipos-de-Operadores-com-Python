e032727d281fa8830dd3fa5d51ce6a4363a70b5f
Thamires <121324998+ranpoluv@users.noreply.github.com>
2024-07-07 20:07:52 -0300

    exemplos.py

---
+++ operadores-aritmeticos/exemplos.py
@@ -0,0 +1,14 @@
+print( 10 -5 * 2)
+# Resultado: 0
+
+print((10 -2) * 2)
+# Resultado: 10
+
+print(10 ** 2 * 2)
+# Resultado: 200
+
+print(10 ** (2 * 2))
+# Resultado: 10000
+
+print(10 / 2 * 4)
+# Resultado: 20.0
