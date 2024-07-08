6799e4149f717fc2760fdc31568235d6aeb5b216
Thamires <121324998+ranpoluv@users.noreply.github.com>
2024-07-07 22:09:42 -0300

    introducao.py

---
+++ operadores-atribuicao/introducao.py
@@ -0,0 +1,37 @@
+# Atribuição simples
+saldo = 500
+print(saldo) # Resultado: 500
+
+# Atribuição com adição
+saldo = 500
+saldo += 200
+
+print(saldo)
+
+# Atribuição com multiplicação
+saldo = 200
+saldo *= 2 # Resultado: 700
+
+print(saldo) # Resultado: 100
+
+# Atribuição com subtração
+saldo = 500
+saldo -= 100
+print(saldo) # Resultado: 400
+
+# Atribuição com divisão
+saldo = 500
+saldo /= saldo
+
+print(saldo) # Resultado: 100.0
+
+saldo = 500
+saldo //= 5
+
+print(saldo) # Resultado: 100
+
+# Atribuição com módulo
+saldo = 500
+saldo %= 480
+
+print(saldo) # Resultado: 20
