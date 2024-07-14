5be5cdbafeeed09bd9bdd1e86bca16c017344467
Thamires <121324998+ranpoluv@users.noreply.github.com>
2024-07-13 21:14:28 -0300

    introducao.py

---
+++ operadores-de-associacao/introducao.py
@@ -0,0 +1,18 @@
+# Exemplo
+curso = "Curso de Python"
+frutas = ["laranja", "uva", "limão"]
+saques = [1500, 100]
+
+"Python" in curso # True
+
+"maçã" not in frutas # True
+
+200 in saques # False
+
+# Video aula
+frutas ["limão", "uva"]
+curso = "Curso de Python"
+
+print("laranja" not in frutas) # Resultado: True
+print("limão" in frutas) # Resultado: True
+print("Python" in curso) # Resultado: True
